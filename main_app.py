3d1e9efc40f776895b6a1bb69621be2105da0272
danieldo21 <80366127+danieldo21@users.noreply.github.com>
2022-02-21 14:34:08 +1300

    Update main_app.py
    added dictionary

--- main_app.py
+++ main_app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
+posts = [
+    
+    {
+      'author':'Daniel Do',      
+      'title':'Blog Post 1',  
+      'content':'First post content',
+      'date_posted':'Febuary 4, 2022'   
+    },
+    
+    {
+      'author':'Danny',      
+      'title':'Blog Post 2',  
+      'content':'Second post content',
+      'date_posted':'Febuary 8, 2022'   
+    }
+    
+]
 
 @app.route("/")
 @app.route("/home")
