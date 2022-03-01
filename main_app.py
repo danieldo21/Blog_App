1deeb510c69013f06918c6a369c9465bb0dd51f1
danieldo21 <80366127+danieldo21@users.noreply.github.com>
2022-03-02 11:44:47 +1300

    Update main_app.py
    added comments and routes for register and login

--- main_app.py
+++ main_app.py
@@ -1,3 +1,4 @@
+from re import L
 from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
 
@@ -34,15 +35,30 @@ posts = [
     
 ]
 
+#Routes
+
+# Route for homepage
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+# Route for about page
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
+# Route for registration page
+@app.route("/register")
+def register():
+  form = RegistrationForm()
+  return render_template('register.html', title='Register', form=form)
+
+# Route for login page
+@app.route("/login")
+def login():
+  form = LoginForm()
+  return render_template('login.html', title='Log In', form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
