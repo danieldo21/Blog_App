621fdb57d958c129a7b4d198070442a23dad3bf3
danieldo21 <80366127+danieldo21@users.noreply.github.com>
2022-05-12 09:30:09 +1200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,5 +1,5 @@
 # This is a blog app that allows users to register and post blogs
-from enum import unique
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
@@ -13,13 +13,22 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.string(20), unique=True, nullable=False)
-  email = db.Column(db.string(120), unique=True, nullable=False)
-  image_file = db.Column(db.string(20), nullable=False, default='default.jpg')
-  password = db.Column(db.string(60), nullable=False)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+  password = db.Column(db.String(60), nullable=False)
   
   def __repr__(self):
-    return f"User('{self.username}','{self.email}','{self.image_file}') "
+    return f"User('{self.username}','{self.email}','{self.image_file}')"
+  
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  content = db.Column(db.Text, nullable=False)
+
+  def __repr__(self):
+    return f"Post('{self.title}','{self.date_posted}')"
 
 # List of post dictionaries
 posts = [
