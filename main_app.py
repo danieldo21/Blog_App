543a65d283e04ffa915d10bb98f35b9110b25e45
danieldo21 <80366127+danieldo21@users.noreply.github.com>
2022-05-31 12:51:46 +1200

    Update main_app.py
    moved to routes.py

--- main_app.py
+++ main_app.py
@@ -12,65 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 
-# List of post dictionaries
-posts = [
-    
-    {
-      'author':'Daniel Do',      
-      'title':'Blog Post 1',  
-      'content':'First post content',
-      'date_posted':'Febuary 4, 2022'   
-    },
-    
-    {
-      'author':'Danny',      
-      'title':'Blog Post 2',  
-      'content':'Second post content',
-      'date_posted':'Febuary 8, 2022'   
-    },
-    
-    {
-      'author':'Danzo',      
-      'title':'Blog Post 3',  
-      'content':'Third post content',
-      'date_posted':'Febuary 16, 2022'   
-    }
-    
-]
-
-#Routes
-
-# Route for homepage
-@app.route("/") #route to main page
-@app.route("/home") #route to home page
-def home():
-    return render_template('home.html', posts=posts) # call to render the home page template
-
-# Route for about page
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
-# Route for registration page
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-      flash(f'Account created for {form.username.data}!', 'success')
-      return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-# Route for login page
-@app.route("/login",  methods=['GET', 'POST'])
-def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-      flash('You have been logged in!', 'success')
-      return redirect(url_for('home'))
-    else:
-      flash('Login Failed. Please check your username and password', 'danger')
-  return render_template('login.html', title='Login', form=form)
 
 # Allows main app to run the web server
 if __name__=='__main__':
