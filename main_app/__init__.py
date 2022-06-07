06ecd69b612b8216417156219100a22155d96b98
danieldo21 <80366127+danieldo21@users.noreply.github.com>
2022-06-08 11:48:00 +1200

    Update __init__.py

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,3 +1,10 @@
 # This is a blog app that allows users to register and post blogs
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e512e9952e4e59843203abecb8e461a2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from main_app import routes
