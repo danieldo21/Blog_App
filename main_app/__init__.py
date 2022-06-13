95d112d6273d6baeef60b0dc868ebe9033515aa2
danieldo21 <80366127+danieldo21@users.noreply.github.com>
2022-06-13 14:27:55 +1200

    Update __init__.py

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,10 +1,13 @@
 # This is a blog app that allows users to register and post blogs
+import bcrypt
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e512e9952e4e59843203abecb8e461a2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from main_app import routes
