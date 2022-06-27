7e7227eb86f68d637819d275ee60a43de973f522
danieldo21 <80366127+danieldo21@users.noreply.github.com>
2022-06-27 14:50:10 +1200

    Update routes.py

--- main_app/routes.py
+++ main_app/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from main_app import app, db, bcrypt
-from main_app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from main_app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from main_app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -112,3 +112,13 @@ def account():
     form.email.data = current_user.email
   image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
   return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+# Route for new posts
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+  form = PostForm()
+  if form.validate_on_submit():
+    flash('Your post has been created', 'success')
+    return redirect(url_for('home'))
+  return render_template('create_post.html', title='New Post', form=form)
